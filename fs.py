cdd8ea105a8a42b0fca70f35e1839c7ef24f4fc3
kacper.gawle@portonmail.com <kacper.gawle@protonmail.com>
2023-01-08 18:39:30 +0100

    small changes in command execution in get_dirlist method

--- fs.py
+++ fs.py
@@ -1,24 +1,26 @@
 import os
 import time
 
-def get_dirlist(dir: str):
-    path = os.path.abspath(dir)
+
+def get_dirlist(directory: str):
+    path = os.path.abspath(directory)
     fs_dir_list_name = '/tmp/fsdirlist' + str(time.time_ns()) + '.txt'
     dir_list = []
-    os.system('du -a ' + path + '| grep -o "\/.*" > ' + fs_dir_list_name)
+    command = 'du -a ' + path + '| grep -o "/.*" > ' + fs_dir_list_name
+    os.system(command)
     file = open(fs_dir_list_name)
     for line in file.readlines():
-        line = line.replace('\n','')
-        line = line.replace(' ','\\ ')
+        line = line.replace('\n', '')
+        line = line.replace(' ', '\\ ')
         if not os.path.isdir(line):
             dir_list.append(line)
     file.close()
     os.remove(fs_dir_list_name)
     return dir_list
 
+
 def main():
     pass
 
 if __name__ == '__main__':
     main()
-
